c55639ebf85fd6e96f03e580089fe0a0977fa6ba
James Gangi <gangijames18@gmail.com>
2026-03-04 13:12:04 -0500

    Initial Commit

---
+++ SafeDial/SafeDialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeDialApp.swift
+//  SafeDial
+//
+//  Created by Jimmy Gangi on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct SafeDialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SafeDialTests/SafeDialTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafeDialTests.swift
+//  SafeDialTests
+//
+//  Created by Jimmy Gangi on 3/4/26.
+//
+
+import Testing
+@testable import SafeDial
+
+struct SafeDialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
